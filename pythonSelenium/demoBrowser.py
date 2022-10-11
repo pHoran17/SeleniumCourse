c41bd012485bdef88bb5ba1b556b221ac6c898ad
Patrick Horan <c16416742@mydit.ie>
2022-10-11 21:42:19 +0100

    2nd commit with updated demoBrowser code, inlcudes more driver commands and comments for chrome usage

--- pythonSelenium/demoBrowser.py
+++ pythonSelenium/demoBrowser.py
@@ -8,11 +8,21 @@ from selenium.webdriver.chrome.service import Service
 service_obj = Service("C:/Users/thund/Documents/ChromeDriver/chromedriver_win32/chromedriver.exe")
 driver = webdriver.Chrome(service=service_obj)
 
+driver.maximize_window()
+
 #Opens browser with specific url
 driver.get("https://rahulshettyacademy.com")
 
 print(driver.title)
 print(driver.current_url)
 
+#Redirects to seleniumPractise page
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+
+driver.minimize_window()
+#Browser controls such as refresh and forward/previous page can be controlled with driver as well
+driver.back()
+driver.refresh()
+driver.forward()
 #Closes browser automatically
 driver.close()
