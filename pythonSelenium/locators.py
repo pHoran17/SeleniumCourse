161fe8e7257f3ed6cb7e251cf5ffa5cd2c5c1021
Patrick Horan <c16416742@mydit.ie>
2022-10-11 23:41:37 +0100

    5th commit with locators file, adds extra element finders and assert condition code

--- pythonSelenium/locators.py
+++ pythonSelenium/locators.py
@@ -22,3 +22,24 @@ driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
 
 #Clicks on specified element(Checkbox in this case)
 driver.find_element(By.ID, "exampleCheck1").click()
+
+#Custom XPATH, used for identifying elements with specified attributes
+# //tagname[@attribute='value'] -> //input[@type='submit']
+
+#Finds element by Css element (input field with name in this case), inputs Paddy into Name input field
+driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Paddy")
+
+#This will submit the form by clicking on the element with a type of submit
+driver.find_element(By.XPATH, "//input[@type='submit']").click()
+
+
+#Text returned by alert from submitting form is stored in string variable here
+message = driver.find_element(By.CLASS_NAME, "alert-success").text
+
+#Message variable printed here
+print(message)
+
+#Checks if characters Success are present in the variable message
+assert "Success" in message
+#Line below triggers error as condition is not met
+#assert "Suc1cess" in message
